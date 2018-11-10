b556434b08c832456b12dd17f7e1b0115e440237
unknown <fantobre@yahoo.com>
2018-11-09 21:58:26 -0800

    updated files

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const path = require('path');
-const express =  require('express');
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 300;
-
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
-app.get('*', (req,res)=>{
+app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(port, ()=>{
-    console.log('Server is up');
+
+app.listen(port, () => {
+  console.log('Server is up!');
 });
